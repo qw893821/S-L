9809ad9af68e6be4c0c24f4b4551e886ddf02766
qw893821 <xiuman893821@gmail.com>
2018-02-17 23:14:53 -0500

    SAA

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    Rigidbody rg;
+    float speed;
+	// Use this for initialization
+	void Start () {
+        rg = GetComponent<Rigidbody>();
+        speed = 2f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h, v;
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+	}
+
+    void Move(float x, float z)
+    {
+        rg.MovePosition(transform.position + new Vector3(x, 0, z).normalized * Time.deltaTime*speed);
+    }
+}
