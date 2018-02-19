f20e080713525d70c663ff57a178945d3bac216a
qw893821 <xiuman893821@gmail.com>
2018-02-19 01:53:59 -0500

    load works
    load works now. know how to solve the issue, but need more time

--- Assets/Scripts/LoadUI.cs
+++ Assets/Scripts/LoadUI.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 
 public class LoadUI : MonoBehaviour {
-
+    Button btn;
 	// Use this for initialization
 	void Start () {
-		
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(LoadGame);
 	}
 	
 	// Update is called once per frame
@@ -19,19 +21,21 @@ public class LoadUI : MonoBehaviour {
 
     void LoadGame()
     {
-        if (File.Exists(Application.persistentDataPath + "/gamesave.save"){
+        if (File.Exists(Application.persistentDataPath + "/gamesave.save")){
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/game.save", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/gamesave.save", FileMode.Open);
             Save save = (Save)bf.Deserialize(file);
             file.Close();
+
+            for (int i = 0; i < TempSave.instance.tempPos.Length; i++)
+            {
+                TempSave.instance.slots[i].transform.position = new Vector3(save.savePosX[i], save.savePosY[i], save.savePosZ[i]);
+                TempSave.instance.slots[i].GetComponent<Renderer>().enabled = save.saveBool[i];
+            }
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(save.savePlayerPosX, save.savePlayerPosY, save.savePlayerPosZ);
+            //SceneManager.LoadSceneAsync(save.savedSceneName, LoadSceneMode.Single);
         }
 
-        for (int i = 0; i < TempSave.instance.tempPos.Length; i++)
-        {
-            TempSave.instance.slots[i].transform.position = new Vector3(save.savePosX,save.savePosY,save.savePosZ);
-            TempSave.instance.slots[i].GetComponent<Renderer>().enabled = save.saveBool;
-        }
-        GameObject.FindGameObjectWithTag("Player").transform.position=new Vector3(save.savePlayerPosX,save.savePlayerPosY,save.savePlayerPosZ);
-        SceneManager.LoadSceneAsync(save.savedSceneName,LoadSceneMode.Single);
+        
     }
 }
